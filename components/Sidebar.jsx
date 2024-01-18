a3656dd880af72c519ea1de04d931ade4cc131f4
Priyadarsh S S <priyadarsh2001@gmail.com>
2024-01-18 20:27:14 +0530

    feat: Add Header and Sidebar components

---
+++ components/Sidebar.jsx
@@ -0,0 +1,45 @@
+import Link from 'next/link'
+import { RxDashboard, RxPerson } from 'react-icons/rx'
+import { HiOutlineShoppingBag } from 'react-icons/hi'
+import { FiSettings } from 'react-icons/fi'
+import { GoHome } from 'react-icons/go'
+
+const Sidebar = ({ children }) => {
+  return (
+    <div className='flex'>
+      <div className='fixed w-20 h-screen p-4 bg-gray-900 border-r-[1px] flex flex-col justify-between'>
+        <div className='flex flex-col items-center'>
+          <Link href='/'>
+            <div className='bg-purple-900 text-white p-3 rounded-lg inline-block'>
+              <GoHome size={20} />
+            </div>
+          </Link>
+          <span className='border-b-[1px] Iborder-gray-200 w-full p-2'></span>
+          <Link href='/'>
+            <div className='bg-gray-800 hover:bg-gray-700 cursor-pointer my-4 p-3 rounded-lg inline-block'>
+              <RxDashboard size={20} />
+            </div>
+          </Link>
+          <Link href='/customers'>
+            <div className='bg-gray-800 hover:bg-gray-700 cursor-pointer my-4 p-3 rounded-lg inline-block'>
+              <RxPerson size={20} />
+            </div>
+          </Link>
+          <Link href='/orders'>
+            <div className='bg-gray-800 hover:bg-gray-700 cursor-pointer my-4 p-3 rounded-lg inline-block'>
+              <HiOutlineShoppingBag size={20} />
+            </div>
+          </Link>
+          <Link href='/'>
+            <div className='bg-gray-800 hover:bg-gray-700 cursor-pointer my-4 p-3 rounded-lg inline-block'>
+              <FiSettings size={20} />
+            </div>
+          </Link>
+        </div>
+      </div>
+      <main className='ml-20 w-full'>{children}</main>
+    </div>
+  )
+}
+
+export default Sidebar
